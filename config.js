a4720b9a4956b3ee633d534752166c7300c532e5
rovinei <ro.vinei@yahoo.com>
2017-08-23 23:25:10 +0700

    add param next in router closure function

---
+++ config.js
@@ -0,0 +1,8 @@
+var cfg = {};
+
+cfg.fb_verify_token = process.env.FB_VERIFY_TOKEN
+cfg.page_access_token = process.env.FB_PAGE_ACCESS_TOKEN
+
+
+
+module.exports = cfg;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/verify', function(req, res){
-    if(req.query['hub.verify_token'] === 'SFn4ULHIwJgqbfQ0ZCGl769ZiA2rfnwJCZPS8WWYfQ0ZCGl7'){
+router.get('/verify', function(req, res, next){
+    if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.fb_verify_token){
+        console.log("Validating webhook");
         res.send(req.query['hub.challenge']);
+    }else{
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
     }
+});
 
-    res.send('Not authorized, 401')
+router.post('/verify', function(req, res, next){
+    let messaging_events = req.body.entry[0].messaging;
+    for(let i = 0; i < messaging_events.length; i++){
+        let event = req.body.entry[0].messaging[i];
+    }
 });
 
 module.exports = router;
